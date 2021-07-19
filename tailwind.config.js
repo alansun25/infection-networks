5cff68b431687b281f1524335d8e68efd89dc0dc
Alan Sun <mralansun@gmail.com>
2021-07-19 18:54:20 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {},
+    fontFamily: {},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
